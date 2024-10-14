7bd059f239a19fa7a9fd26fb5c34a8664386415f
mgkraynova <Marina.Kraynova@leroymerlin.ru>
2024-10-14 09:23:11 +0300

    update Resources class

--- src/Resource.ts
+++ src/Resource.ts
@@ -1,27 +1,43 @@
-class Resources {
-    constructor() {
-        this.toLoad = {
-            "sky": '../public/sprites/sky.png',
-            "ground": '../public/sprites/ground.png',
-            "hero": '../public/sprites/hero.png',
-            "shadow": '../public/sprites/shadow.png',
-        }
-
-        this.images = {}
-
-        Object.keys(this.toLoad).forEach((key) => {
-            const image = new Image();
-
-            image.src = this.toLoad[key];
-
-            this.images[key] = {
-                image,
-                isLoaded: false
-            };
-
-            image.onload = () => {
-                this.images[key].isLoaded = true;
-            }
-        })
-    }
+export type TImage = {
+  [key: string]: {
+    image: CanvasImageSource;
+    isLoaded: boolean;
+  };
 }
+
+export class Resources {
+  toLoad: {
+    [key: string]: string;
+  };
+  images: TImage;
+
+  constructor() {
+    this.toLoad = {
+      sky: 'sprites/sky.png',
+      ground: 'sprites/ground.png',
+      hero: 'sprites/hero-sheet.png',
+      shadow: 'sprites/shadow.png',
+    };
+
+    this.images = {};
+
+    Object.keys(this.toLoad).forEach((key) => {
+      const image = new Image();
+
+      image.src = this.toLoad[key];
+
+      // @ts-ignore
+      this.images[key] = {
+        image,
+        isLoaded: false,
+      };
+
+      image.onload = () => {
+        // @ts-ignore
+        this.images[key].isLoaded = true;
+      };
+    });
+  }
+}
+
+export const resources = new Resources();
